1c1be3872535b67f05404973c268da1cec14b944
Max Tischberger <83185715+CxlledMax@users.noreply.github.com>
2021-11-27 23:39:29 +0100

    Update csvhandler.py

--- csvhandler.py
+++ csvhandler.py
@@ -1,13 +1,15 @@
 class CSVHandler:
     def load(path):
-        file = open(path)
-        beautified = [element for element in [row.strip() for row in file.readlines()]]
-        final = [row.split(", ") for row in beautified]
-        return final
+        if ".csv" in path:
+            file = open(path)
+            beautified = [element for element in [row.strip() for row in file.readlines()]]
+            final = [row.split(", ") for row in beautified]
+            return final
 
     def parse(path, data):
-        with open(path, "r+") as file:
-            file.truncate(0)
-        with open(path, "a") as file:
-            for row in data:
-                file.write(", ".join(row) + "\n")
+        if ".csv" in path:
+            with open(path, "r+") as file:
+                file.truncate(0)
+            with open(path, "a") as file:
+                for row in data:
+                    file.write(", ".join(row) + "\n")
